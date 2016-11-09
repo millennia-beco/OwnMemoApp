19e6200c4752ef1ba324df7ec28f28f2b5fd9e92
安部航大 <abeko@dbms.cs.kumamoto-u.ac.jp>
2016-11-09 20:11:29 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/kumamoto_u/abco/ownmemoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.kumamoto_u.abco.ownmemoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
